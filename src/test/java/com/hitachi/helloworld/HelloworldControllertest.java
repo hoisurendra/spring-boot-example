7aad172fbca659855ede56524bf5e2c4342a4b95
hoisurendra <52737319+hoisurendra@users.noreply.github.com>
2024-07-30 12:45:23 +0530

    Update HelloworldControllertest.java

--- src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
+++ src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
@@ -2,17 +2,14 @@
 package com.hitachi.helloworld;
 
 // import com.hitachi.helloworld.HellowWorldController;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc; 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,11 +27,11 @@ public class HelloworldControllertest {
  
     // @MockBean
     // private AdminService adminService;
-  
-     @Test
-     public void testPrintHello() throws Exception {        
-      mockMvc.perform(get("/welcome"))
-       .andExpect(status().isOk())
-       .andExpect(content().string("Hi Hello, Welcome to Web Page deployed using Jenkins Pipeline & Docker")); 
+  @Test
+    public void testPrintHello() throws Exception {        
+        mockMvc.perform(get("/welcome")
+                .contentType(MediaType.TEXT_PLAIN))
+               .andExpect(status().isOk())
+               .andExpect(content().string("Hi Hello, Welcome to Web Page deployed using Jenkins Pipeline & Docker")); 
     }
 }
