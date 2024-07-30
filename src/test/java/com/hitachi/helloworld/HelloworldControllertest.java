d61f58f3c403f423fb45fceb2bfdf8ae17c80a3b
hoisurendra <52737319+hoisurendra@users.noreply.github.com>
2024-07-30 12:32:16 +0530

    Update HelloworldControllertest.java

--- src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
+++ src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
@@ -6,6 +6,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc; 
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
 
 // @SpringBootTest
 // @AutoConfigureMockMvc
@@ -14,7 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 public class HelloworldControllertest { 
  
     @Autowired
-    public MockMvc mockMvc;
+    private MockMvc mockMvc;
  
     // @MockBean
     // private AdminService adminService;
