5cc2f11452e83198b82f9236e28c02d0c224cf74
hoisurendra <52737319+hoisurendra@users.noreply.github.com>
2024-07-30 12:16:16 +0530

    Update HelloworldControllertest.java

--- src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
+++ src/test/java/com/hitachi/helloworld/HelloworldControllertest.java
@@ -2,11 +2,15 @@
 package com.hitachi.helloworld;
  
 import org.springframework.test.web.servlet.MockMvc;
- 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc; 
 
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(HelloWorldController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+// @ExtendWith(SpringExtension.class)
+// @WebMvcTest(HelloWorldController.class)
 public class HelloworldControllertest {
 
   
@@ -19,4 +23,7 @@ public class HelloworldControllertest {
   
      @Testpublic
     void testPrintHello() throwsException {        
-      mockMvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string("Hi Hello, Welcome to Web Page deployed using Jenkins Pipeline & Docker")); }
+      mockMvc.perform(get("/hello"))
+       .andExpect(status()
+       .isOk()).andExpect(content()
+       .string("Hi Hello, Welcome to Web Page deployed using Jenkins Pipeline & Docker")); }}
